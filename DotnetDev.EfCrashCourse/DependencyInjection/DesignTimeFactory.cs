6a3240fa6c26be42f2b5411a7d28436a1db15e56
kgiszewski <kevin@giszewski.com>
2025-05-11 14:30:16 -0400

    Add Migration

---
+++ DotnetDev.EfCrashCourse/DependencyInjection/DesignTimeFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace DotnetDev.EfCrashCourse.DependencyInjection;
+
+/// <summary>
+/// This class is needed to handle migrations since we don't have a host builder
+///
+/// https://learn.microsoft.com/en-us/ef/core/cli/dbcontext-creation?tabs=dotnet-core-cli#from-a-design-time-factory
+/// </summary>
+public class DesignTimeFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        
+        optionsBuilder.UseNpgsql($"Host=localhost:5432;Database=postgres;Username=postgres;Password=postgres;")
+            .UseSnakeCaseNamingConvention();
+        
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
---
+++ DotnetDev.EfCrashCourse/Migrations/20250511182728_AddOrders.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotnetDev.EfCrashCourse.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "billing");
+
+            migrationBuilder.CreateTable(
+                name: "customers",
+                schema: "billing",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    first_name = table.Column<string>(type: "text", nullable: false),
+                    last_name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_customers", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "orders",
+                schema: "billing",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    customer_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    quantity = table.Column<int>(type: "integer", nullable: false),
+                    amount = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_orders", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_orders_customers_customer_id",
+                        column: x => x.customer_id,
+                        principalSchema: "billing",
+                        principalTable: "customers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_orders_customer_id",
+                schema: "billing",
+                table: "orders",
+                column: "customer_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "orders",
+                schema: "billing");
+
+            migrationBuilder.DropTable(
+                name: "customers",
+                schema: "billing");
+        }
+    }
+}
